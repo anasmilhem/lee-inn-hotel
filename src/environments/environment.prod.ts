d58ec3d74debb5c3bfb73af55d337ef5f5f09af5
Anas Milhem <anasmilhem1997@gmail.com>
2023-04-20 12:52:11 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,1 @@
-export const environment = {
-  firebase: {
-    projectId: 'arlingtoninn',
-    appId: '1:798788720685:web:ec6c7e73df00fff190c0a4',
-    storageBucket: 'arlingtoninn.appspot.com',
-    apiKey: 'AIzaSyC64EI36WoGxqlBa9YXP3deOXzXlQT3hOE',
-    authDomain: 'arlingtoninn.firebaseapp.com',
-    messagingSenderId: '798788720685',
-    measurementId: 'G-Q1P5BSL25L',
-  },
-  production: true
-};
+
